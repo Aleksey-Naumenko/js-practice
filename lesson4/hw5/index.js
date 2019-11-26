fe320f3471d43ae29d60e86e05c0ef6a3641eeb2
Aleksey <aleksey_naumenko@ukr.net>
2019-11-26 23:08:28 +0200

    1

--- lesson4/hw5/index.js
+++ lesson4/hw5/index.js
@@ -5,6 +5,6 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 let num = Math.floor(factorial / 1234);
-let withoutRest = num * 1234;
-let result =  factorial - withoutRest;
+let numWithoutRest = num * 1234;
+let result =  factorial - numWithoutRest;
 num > result ? console.log(true) : console.log(false);
