b5cd7e11b0cb6443c6f051da3b954b80a4f9c61d
Aleksey <aleksey_naumenko@ukr.net>
2019-11-26 21:36:53 +0200

    hw2

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -1,13 +1,21 @@
-const m = 10;
-const n = 20;
+const m = 20;
+const n = 30;
 
 let result = 0;
 
 for (let i = m; i <= n; i++) {
-    if (i % 2 === 0 && i % 4 !== 0) result += i;
-    else if (i % 3 === 0) result -= i;
-    else if (i % 4 === 0) result *= i;
-    if (i % 5 === 0) console.log(i);
+
+    if (i % 2 === 0 && i % 4 !== 0) {
+        result += i;
+    } else if (i % 3 === 0) {
+        result -= i;
+    } else if (i % 4 === 0) {
+        result *= i;
+    }
+
+    if (i % 5 === 0) {
+        console.log(i);
+    }
 }
 
 console.log(result);
