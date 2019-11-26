ef7a21c26244250682acd200486c4485068ebf65
Aleksey <aleksey_naumenko@ukr.net>
2019-11-26 09:29:09 +0200

    1

---
+++ lesson1/lesson2/task1/index.js
@@ -0,0 +1,9 @@
+
+const userName = 'John';
+const greeting = "I'm John";
+const _profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDssert = 'cake';
+let pet_ = 'dog';
+let superNumber;
+superNumber = 17;
