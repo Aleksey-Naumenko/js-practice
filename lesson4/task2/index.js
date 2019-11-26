f1dbef4943a12b34272c80b446eb268349eae5f3
Aleksey <aleksey_naumenko@ukr.net>
2019-11-26 20:37:55 +0200

    2

--- lesson4/task2/index.js
+++ lesson4/task2/index.js
@@ -1,11 +1,11 @@
-let m = 40;
-let n = 191;
+const m = 40;
+const n = 191;
 
 let result = 1;
 
 for (let i = m; i <= n; i++) {
     if (i % 2 === 1) {
-        result += i;
+        result *= i;
     }
 }
 
