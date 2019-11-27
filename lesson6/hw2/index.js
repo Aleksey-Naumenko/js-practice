93ba88aaee07d2151056c6ca9a2e12f48b2cc8c3
Aleksey <aleksey_naumenko@ukr.net>
2019-11-27 13:53:49 +0200

    hw2

---
+++ lesson6/hw2/index.js
@@ -0,0 +1,8 @@
+function reverseArray(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+    return arr.reverse();
+}
+
+console.log(reverseArray([1, 3, 4,5 ,6,7,7,8,9,,09,8,776,5,43,]));
