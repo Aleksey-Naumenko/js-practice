1a54b6b2dacc5ee8d645e26b9072fba5e06ce9ea
Aleksey <aleksey_naumenko@ukr.net>
2019-11-27 17:01:40 +0200

    hw6

--- lesson6/hw6/index.js
+++ lesson6/hw6/index.js
@@ -2,7 +2,6 @@ function withdraw(clients, balances, client, amount) {
     if (balances[clients.indexOf(client)] < amount) {
         return -1;
     }
-    let restOnBalance = balances[clients.indexOf(client)] - amount;
-    return restOnBalance;
+    return balances[clients.indexOf(client)] - amount;
 }
 
