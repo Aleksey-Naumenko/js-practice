cc3d6722650ac5f4fba6d1160fb5f07433166a6b
Aleksey <aleksey_naumenko@ukr.net>
2019-11-27 13:26:19 +0200

    2

--- lesson6/task2/index.js
+++ lesson6/task2/index.js
@@ -1,5 +1,10 @@
 function getSum(array) {
     let sum = 0;
+
+    if (!Array.isArray(array)) {
+        return null;
+    }
+    
     for (let i = 0; i < array.length; i++) {
         sum += i;
     }
