832e2391e61dfd02703b50171c852062a0b197a2
Aleksey <aleksey_naumenko@ukr.net>
2019-11-28 16:26:25 +0200

    hw1

--- lesson9/hw1/index.js
+++ lesson9/hw1/index.js
@@ -5,8 +5,28 @@ function getCustomersList(obj) {
     }
 
     let sortedObj = Object.values(obj);
-    console.log(sortedObj);
+    // console.log(sortedObj);
 
     let sortedByAge = sortedObj.sort((person, secondPerson) => person.age - secondPerson.age);
-    console.log(sortedByAge);
+    return sortedByAge;
+    // console.log(sortedByAge);
 }
+
+// console.log(getCustomersList( {
+//     'customer-id-1': {
+//         name: 'William',
+//         age: 54
+//     },
+//     'customer-id-2': {
+//         name: 'Tom',
+//         age: 17
+//     },
+//     'customer-id-3': {
+//         name: 'Bob',
+//         age: 30
+//     },
+//     'customer-id-4': {
+//         name: 'David',
+//         age: 44
+//     },
+// }));
