79e97eb02c5c05ecff11df879fab013b414d8d76
Aleksey <aleksey_naumenko@ukr.net>
2019-11-28 15:11:40 +0200

    2

--- lesson9/task1/index.js
+++ lesson9/task1/index.js
@@ -2,6 +2,5 @@ function getAdults(obj) {
     let res = Object.entries(obj)
         .filter(person => person[1] >= 18)
         .map(name => name[0]);
-    // console.log(nameArr);
     return res;
 }
