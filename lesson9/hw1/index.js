b3185deb9f56787b470238d4783e639ccbc26ecf
Aleksey <aleksey_naumenko@ukr.net>
2019-11-28 23:25:37 +0200

    hw1

--- lesson9/hw1/index.js
+++ lesson9/hw1/index.js
@@ -1,31 +1,16 @@
 function getCustomersList(obj) {
-    let myObj = Object.entries(obj);
-
-    myObj.map(function(item) {
-        item[1].id = item[0];
-        item.splice(0, 1);
-    });
-
-    let sortedByAge = myObj.sort((person, nextPerson) => person.age - nextPerson.age);
     
-    return sortedByAge;
-}
+    let arrayOfArrays =  Object.entries(obj);
 
-console.log(getCustomersList( {
-    'customer-id-1': {
-        name: 'William',
-        age: 54
-    },
-    'customer-id-2': {
-        name: 'Tom',
-        age: 17
-    },
-    'customer-id-3': {
-        name: 'Bob',
-        age: 30
-    },
-    'customer-id-4': {
-        name: 'David',
-        age: 44
-    },
-}));
+    arrayOfArrays.map(function(item) {
+            item[1].id = item[0];
+            item.splice(0, 1);
+        });
+
+    let arrayOfObjects = arrayOfArrays.reduce((acc, elem) => {
+        return acc.concat(elem);
+        }, [])
+        .sort((person, nextPerson) => person.age - nextPerson.age);
+
+    return arrayOfObjects;
+}
