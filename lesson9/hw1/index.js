c8d5ec3d98638c5b67e722cebedad84c2fdafdf6
Aleksey <aleksey_naumenko@ukr.net>
2019-11-28 16:25:27 +0200

    hw1

--- lesson9/hw1/index.js
+++ lesson9/hw1/index.js
@@ -10,22 +10,3 @@ function getCustomersList(obj) {
     let sortedByAge = sortedObj.sort((person, secondPerson) => person.age - secondPerson.age);
     console.log(sortedByAge);
 }
-
-console.log(getCustomersList( {
-    'customer-id-1': {
-        name: 'William',
-        age: 54
-    },
-    'customer-id-2': {
-        name: 'Tom',
-        age: 17
-    },
-    'customer-id-3': {
-        name: 'Bob',
-        age: 30
-    },
-    'customer-id-4': {
-        name: 'David',
-        age: 44
-    },
-}));
