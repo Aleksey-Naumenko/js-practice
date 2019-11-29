590225a987488338052ab28d3b533b51e665cae2
Aleksey <aleksey_naumenko@ukr.net>
2019-11-29 19:03:03 +0200

    hw2

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -1,13 +1,13 @@
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
-function getRandomNumbers(length, start, end) {
+function getRandomNumbers(length, min, max) {
     let arrayOfNumbs = [];
 
-    if (Number.isInteger(start, end) && end > start) {
+    if (Number.isInteger(min, max) && max > min) {
         for (let i = 1; i <= length; i++) {
-            arrayOfNumbs.push(getRandomNumber(start, end));
+            arrayOfNumbs.push(getRandomNumber(min, max));
         } 
     } else {
         return null;
