86f5f646c3d3fa6e86b5459df0c6e6ebadc80fe4
Aleksey <aleksey_naumenko@ukr.net>
2019-11-29 16:57:54 +0200

    4

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,4 +1,7 @@
 function getMaxAbsoluteNumber(arr) {
+    if (!Array.isArray(arr) && arr.length == 0) {
+        return null;
+    }
     let absArr = arr.map(item => Math.abs(item));
     let theBiggestNum = Math.max(...absArr);
     return theBiggestNum;
