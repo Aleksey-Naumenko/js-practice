b6035f376e2fd367c6765037fc0cea819e9c2b18
Aleksey <aleksey_naumenko@ukr.net>
2019-11-29 23:54:02 +0200

    hw2

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -1,11 +1,10 @@
 function getRandomNumbers(length, min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
     let arrayOfNumbs = [];
     
     if (max < min || max - min < 0) return null;
-        
-    min = Math.floor(min);
-    max = Math.floor(max);
-
+    
     do {
         arrayOfNumbs.push(Math.floor(Math.random() * (max - min) + min));
     } while (arrayOfNumbs.length < length);
