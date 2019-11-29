8565dbaf0e8dfa01ae565bd5dfcda6fec4e4ef14
Aleksey <aleksey_naumenko@ukr.net>
2019-11-29 17:58:18 +0200

    hw2

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -4,10 +4,12 @@ function getRandomNumber(min, max) {
 
 function getRandomNumbers(length, start, end) {
     let arrayOfNumbs = [];
-    if (parseInt(end) > parseInt(start)) {
+    if (Number.isInteger(end) > Number.isInteger(start)) {
         for (let i = 1; i <= length; i++) {
             arrayOfNumbs.push(getRandomNumber(start, end));
-        }
+        } 
+    } else {
+        return null;
     }
     return arrayOfNumbs;
 }
