bb5b96ba1a035ddfb5475f36ac8ea340d3b3d877
Aleksey <aleksey_naumenko@ukr.net>
2019-11-30 13:30:38 +0200

    hw2

--- lesson11/hw2/index.js
+++ lesson11/hw2/index.js
@@ -2,10 +2,9 @@ function countOccurrences(str, substr) {
     if (substr.length === 0) return null;
 
     let counter = -1;
-    let res = str.split(substr);
-    for (let i = 0; i < res.length; i++) {
-        counter += 1;
-    }
+    str.split(substr)
+        .map(() => counter += 1);
+
     return counter;
 }
 
