327d87fd828f48a565e4932cdadcdd84fbaabd67
Aleksey <aleksey_naumenko@ukr.net>
2019-11-30 23:45:17 +0200

    hw2

---
+++ lesson13/hw2/calc.test.js
@@ -0,0 +1,26 @@
+import { calc } from './calc';
+
+it ('should get sum of numbers', () => {
+    const result = calc('2 + 2');
+    expect(result).toEqual('2 + 2 = 4');
+});
+
+it ('should substract numbers', () => {
+    const result = calc('4 - 2');
+    expect(result).toEqual('4 - 2 = 2');
+});
+
+it ('should multiply the numbers', () => {
+    const result = calc('2 * 2');
+    expect(result).toEqual('2 * 2 = 4');
+});
+
+it ('should divide the numbers', () => {
+    const result = calc('2 / 2');
+    expect(result).toEqual('2 / 2 = 1');
+});
+
+it ('should get null - invalid input', () => {
+    const result = calc(2 + 2);
+    expect(result).toEqual(null);
+});
