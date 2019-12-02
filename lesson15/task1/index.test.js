c2ddeb4be777f0363a8efd526b9ed637b8e1fe5c
Aleksey <aleksey_naumenko@ukr.net>
2019-12-02 16:17:50 +0200

    1

--- lesson15/task1/index.test.js
+++ lesson15/task1/index.test.js
@@ -1,7 +1,14 @@
-// import { makeCounter } from './index';
+import { makeCounter } from './index';
 
-// it ('get counter started from 0', () => {
+it ('get counter started from 0', () => {
+    let counter = makeCounter();
+    let result = counter();
+    expect(result).toEqual(0);
+})
 
-//     let result = makeCounter();
-//     expect(result).toEqual(0);
-// })
+it ('Second step - get counter 1', () => {
+    let counter = makeCounter();
+    counter();
+    let result = counter();
+    expect(result).toEqual(1);
+})
