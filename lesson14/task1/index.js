6754439b63ffa10f89a43b6bef8f9ced2e10f9d7
Aleksey <aleksey_naumenko@ukr.net>
2019-12-02 11:30:37 +0200

    1

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -1,11 +1,11 @@
-let message = 'Hello';
+let message = 'Just learn it!';
 
 export function sendMessage(name) {
     let sender = 'Gromcode';
-    console.log(`${name}, ${message}! Your ${sender}`);
+    console.log(`${name}, ${message}. Your ${sender}`);
 }
 
-export function setMessage(newText) {
-    message = newText;
+export function setMessage(text) {
+    message = text;
 }
 
