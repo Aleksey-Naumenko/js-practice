14fec8125b19893528df604a489410eda4384785
Aleksey <aleksey_naumenko@ukr.net>
2019-12-03 14:51:17 +0200

    hw1

---
+++ lesson16/hw1/index.js
@@ -0,0 +1,15 @@
+function createArrayOfFunctions(len) {
+    if (typeof len !== 'number') {
+        return null;
+    }
+
+    let arr = [];
+
+    for (let i = 0; i < len; i++) {
+        arr[i] = function() {
+            return i;
+        }
+    }
+
+    return arr;
+}
