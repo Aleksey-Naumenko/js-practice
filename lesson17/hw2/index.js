222efed1183e6d9dc8797704dd5c4e64fc1209de
Aleksey <aleksey_naumenko@ukr.net>
2019-12-04 19:31:31 +0200

    hw2

--- lesson17/hw2/index.js
+++ lesson17/hw2/index.js
@@ -23,7 +23,7 @@ export const timer = {
     },
 
     getTime() {
-        return this.secondsPassed > 10 ? 
+        return this.secondsPassed < 10 ? 
             `${this.minsPassed}:0${this.secondsPassed}` :
             `${this.minsPassed}:${this.secondsPassed}`;
     }
