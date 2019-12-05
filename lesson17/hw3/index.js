55bb41d9b7d8221c9d4e4688930ceafb8388b7cf
Aleksey <aleksey_naumenko@ukr.net>
2019-12-05 12:56:57 +0200

    hw3

--- lesson17/hw3/index.js
+++ lesson17/hw3/index.js
@@ -3,16 +3,3 @@ export function bind(func, context, ...args) {
         return func.call(context, ...thisArgs, ...args);
     }
 }
-
-// function showFullName(street) {
-//     console.log(`My full name is ${this.name} ${this.lastName}.
-//         I live on ${street}`);
-// }
-
-// let user = {
-//     name: 'Bob',
-//     lastName: 'Marley',
-// }
-
-// let firstUser = bind(showFullName, user);
-// console.log(firstUser('MainRoad, 26'));
---
+++ lesson17/hw3/index.test.js
@@ -0,0 +1,27 @@
+import { bind } from './index';
+
+it ('sould get full-name and adress info', () => {
+    function showFullName(street) {
+        return `My full name is ${this.name} ${this.lastName}. I live on ${street}`;
+    }
+    let user = {
+        name: 'Bob',
+        lastName: 'Marley',
+    }
+    let firstUser = bind(showFullName, user);
+    let result = firstUser('MainRoad, 26');
+    expect(result).toEqual('My full name is Bob Marley. I live on MainRoad, 26');
+});
+
+it ('get full-name and undefined street (didn"t pass the parameter)', () => {
+    function showFullName(street) {
+        return `My full name is ${this.name} ${this.lastName}. I live on ${street}`;
+    }
+    let user = {
+        name: 'Bob',
+        lastName: 'Marley',
+    }
+    let firstUser = bind(showFullName, user);
+    let result = firstUser(); // no parameter
+    expect(result).toEqual('My full name is Bob Marley. I live on undefined');
+});
