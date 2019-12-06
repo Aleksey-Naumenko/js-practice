69e6e2d8e53ce182095508316fa0af4bbaef86f5
Aleksey <aleksey_naumenko@ukr.net>
2019-12-06 14:22:54 +0200

    hw3

--- lesson17/hw3/index.test.js
+++ lesson17/hw3/index.test.js
@@ -25,3 +25,17 @@ it ('get full-name and undefined street (didn"t pass the parameter)', () => {
     let result = firstUser(); // no parameter
     expect(result).toEqual('My full name is Bob Marley. I live on undefined');
 });
+
+it ('get changed last-name and undefined street (didn"t pass the parameter)', () => {
+    function showFullName(street) {
+        return `My full name is ${this.name} ${this.lastName}. I live on ${street}`;
+    }
+    let user2 = {
+        name: 'Aleksey',
+    }
+    let firstUser = bind(showFullName, user2);
+    user2.lastName = 'Naumenko';
+    let result = firstUser(); // no parameter
+    expect(result).toEqual('My full name is Aleksey Naumenko. I live on undefined');
+});
+
