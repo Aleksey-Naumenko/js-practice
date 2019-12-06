da513363444beedf6e69cc6c6e0d6d4dd8918ca3
Aleksey <aleksey_naumenko@ukr.net>
2019-12-06 13:45:40 +0200

    hw1

--- lesson18/hw1/index.js
+++ lesson18/hw1/index.js
@@ -1,9 +1,11 @@
 export function spy(func) {
 	let calls = [];
-    let t =  function spied(...args) {
+    let t =  function(...args) {
         calls.push(Array.from(args));
+        // t.apply(func, [args]);
         t.bind(func, args);
-	}
+        return func.call(this, ...args);
+	};
     t.calls = calls;
     return t;
 }
