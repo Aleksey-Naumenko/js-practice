4f68712b08f149d5f86107a64047c08792686712
Aleksey <aleksey_naumenko@ukr.net>
2019-12-07 15:14:35 +0200

    4

---
+++ lesson19/task4/idex.js
@@ -0,0 +1,22 @@
+export function User(name, age) {
+    this.name = name;
+    this.age = age;
+
+}
+
+User.prototype.sayHi = function() {
+    console.log(`Hi, I am ${this.name}`);
+}
+
+User.prototype.requestNewPhoto = function() {
+    console.log(`New photo request was sent for ${this.name}`);
+}
+
+User.prototype.setAge = function(age) {
+    if (age < 0) return false;
+    if (age >= 25) {
+        console.log(`New photo request was sent for ${this.name}`);
+    };
+    this.age = age;
+    return this.age;
+}
