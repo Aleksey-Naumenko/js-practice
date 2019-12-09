bc6b79f89ddaa90d4ef2b22a09d94b098151907a
Aleksey <aleksey_naumenko@ukr.net>
2019-12-09 22:18:03 +0200

    3

---
+++ lesson20/task3/index.js
@@ -0,0 +1,18 @@
+export class Wallet {
+    constructor() {
+        this.balance = 0;
+    }
+    getBalance() {
+        return this.balance;
+    }
+    deposit(depo) {
+        return this.balance += depo;
+    }
+    withdraw(withdr) {
+        if (this.balance > withdr) {
+            console.log('No enough funds');
+            return;
+        }
+        return this.balance -= withdr;
+    }
+}
