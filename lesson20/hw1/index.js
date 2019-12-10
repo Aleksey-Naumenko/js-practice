007a1a70e176f96b5384526f010847823a8eb027
Aleksey <aleksey_naumenko@ukr.net>
2019-12-10 12:07:35 +0200

    hw1

---
+++ lesson20/hw1/index.js
@@ -0,0 +1,26 @@
+export class Order {
+    constructor(price, city, type) {
+        this.id = Math.random().toString();
+        this.price = price;
+        this.dateCreated = new Date();
+        this.isConfirmed = false;
+        this.dateConfirmed = new Date();
+        this.city = city;
+        this.type = type;
+    }
+    checkPrice(price) {
+        return price > 1000;
+    }
+    confirmOrder() {
+        if (this.isConfirmed == false) {
+            this.isConfirmed = true;
+        }
+        this.dateConfirmed = new Date();
+    }
+    isValidType() {
+        if (this.type !== 'Buy' || this.type !== 'Sell') {
+            return false;
+        }
+        return true;
+    }
+}
