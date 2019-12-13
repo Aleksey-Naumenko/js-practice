79df527b44be5b7b379e274a43c54702644c1ecb
Aleksey <aleksey_naumenko@ukr.net>
2019-12-13 12:48:01 +0200

    hw1

--- lesson22/hw1/index.js
+++ lesson22/hw1/index.js
@@ -1,10 +1,17 @@
 const tasks = [
-    { text: 'Buy milk', done: false, date: new Date(), },
-    { text: 'Pick up Tom from airport', done: false, date: new Date(), },
-    { text: 'Visit party', done: false, date: new Date(), },
-    { text: 'Visit doctor', done: true, date: new Date(), },
-    { text: 'Buy meat', done: true, date: new Date(), },
+    { text: 'Buy milk', done: false, date: new Date(), id: getRandomId()},
+    { text: 'Pick up Tom from airport', done: false, date: new Date(), id: getRandomId()},
+    { text: 'Visit party', done: false, date: new Date(), id: getRandomId()},
+    { text: 'Visit doctor', done: true, date: new Date(), id: getRandomId()},
+    { text: 'Buy meat', done: true, date: new Date(), id: getRandomId()},
 ];
+
+
+function getRandomId() {
+	return Math.floor(Math.random() * 10000);
+}
+
+
 
 const renderListItems = listItems => {
     const list = document.querySelector('.list');
@@ -12,7 +19,7 @@ const renderListItems = listItems => {
 
     const listElements = listItems
         .sort((a, b) => a.done - b.done)
-        .map( ({ text, done }) => {
+        .map( ({ text, done, id = getRandomId() }) => {
             const listElement = document.createElement('li');
             listElement.classList.add('list__item');
             if (done) {
@@ -20,6 +27,7 @@ const renderListItems = listItems => {
             }
             const checkboxElement = document.createElement('input');
             checkboxElement.setAttribute('type', 'checkbox');
+            checkboxElement.setAttribute('data-id', id);
             checkboxElement.checked = done;
             checkboxElement.classList.add('list__item-checkbox');
             listElement.append(checkboxElement, text);
@@ -31,50 +39,31 @@ const renderListItems = listItems => {
 }
 renderListItems(tasks);
 
-// const updatingList = (elem) => {
-//     if (elem.onchange) {
-//         tasks[i].done == true;
-//     } else {
-//         tasks[i].done == false;
-//     }
-//     renderListItems(tasks);
-// }
 
-// ------------------DON'T UNDERSTAND HERE --------------------------------
 
-let list = Array.from(document.querySelectorAll('.list__item-checkbox'));
-for (let i = 0; i < list.length; i++) {
-    list[i].addEventListener('change', () => {
-        for (let i = 0; i < tasks.length; i++) {
-            if (list[i].chacked) {
-                tasks[i].done == true;
-            } else {
-                tasks[i].done == false;
-            }
-            console.log(tasks);
+const listWithTasks = document.querySelector('.list');
+const updateTasksList = event => {        
+        const isCheckbox = event.target.classList.contains('list__item-checkbox');
+        if (!isCheckbox) {
+            return;
         }
-    });
-    renderListItems(tasks);
+        console.log(tasks.map(item => item.id));
+        const taskData = tasks.find(task => task.id == event.target.dataset.id);
+        Object.assign(taskData, { done: event.target.checked });
+        renderListItems(tasks);
 }
+const updateTasksListHandler = listWithTasks.addEventListener('click', updateTasksList);
 
-//------------------------------------------------------------------------
+
 
 const creteBtn = document.querySelector('.create-btn');
 const addNewTask = () => {
     const input = document.querySelector('.task-input');
+
     if (!input.value) return;
-    tasks.unshift({text: `${input.value}`, done: false, date: new Date(),});
-    console.log(tasks);
+    tasks.unshift( {text: `${input.value}`, done: false, date: new Date(), id: getRandomId() });
     input.value = '';
+
     renderListItems(tasks);
 }
 const addNewTaskHandler = creteBtn.addEventListener('click', addNewTask);
-
-//   При клике на чекбокс статус дела должен переключаться между 
-//   выполнено / не выполнено. Соответственно список должен отобразить 
-//   новое состояние дел
-
-
-//       Элементы списка должны быть отсортированы по 
-//       дате создания внутри своих категорий (не выполненные сверху,
-//          выполненные внизу)
