5e1587a6d30ed7662eb3cf6727d62897a66d444f
Aleksey <aleksey_naumenko@ukr.net>
2019-12-15 21:56:43 +0200

    hw1

--- lesson24/hw1/index.js
+++ lesson24/hw1/index.js
@@ -1,5 +1,5 @@
 export const getDiff = (startDate, endDate) => {
-    let timeStamp = new Date(endDate) - new Date(startDate);
+    let timeStamp = endDate - startDate;
 
     let permRest;
     let diff = [];
