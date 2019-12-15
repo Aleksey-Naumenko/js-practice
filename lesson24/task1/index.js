918fd01a818c6916d6d99980445a17b58ee0a211
Aleksey <aleksey_naumenko@ukr.net>
2019-12-15 15:33:48 +0200

    1

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,7 +1,8 @@
 export const dayOfWeek = (date, days) => {
-    const weekDays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
     const day = new Date(date).getDate();
     const futureDate = new Date(date).setDate(day + days);
 
     return weekDays[new Date(futureDate).getDay()];
 }
+
+const weekDays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
