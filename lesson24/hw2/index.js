e3b131d1c42e46ed5c34388790800b4186cf948f
Aleksey <aleksey_naumenko@ukr.net>
2019-12-16 10:41:34 +0200

    hw2

--- lesson24/hw2/index.js
+++ lesson24/hw2/index.js
@@ -6,7 +6,7 @@ export function studentsBirthDays(students) {
 
     let arrOfStudents = students
         .sort((a, b) => a.birthDate.split('/')[1] - b.birthDate.split('/')[1])
-        
+
         .forEach(student => {
         let studentMonth = new Date(student.birthDate).getMonth();
         let numbOfMonth = months[studentMonth];
@@ -21,13 +21,13 @@ export function studentsBirthDays(students) {
     return final;
 }
 
-let students = [
-    { name: 'Tom', birthDate: '01/15/2010' },
-    { name: 'Kom', birthDate: '01/16/2010' },
-    { name: 'Jom', birthDate: '02/17/2010' },
-    { name: 'Wom', birthDate: '01/11/2010' },
-    { name: 'Qom', birthDate: '02/19/2010' },
-    { name: 'Rom', birthDate: '01/09/2010' },
-    { name: 'Som', birthDate: '03/21/2010' },
-    { name: 'Dom', birthDate: '04/22/2010' },
-]
+// let students = [
+//     { name: 'Tom', birthDate: '01/15/2010' },
+//     { name: 'Kom', birthDate: '01/16/2010' },
+//     { name: 'Jom', birthDate: '02/17/2010' },
+//     { name: 'Wom', birthDate: '01/11/2010' },
+//     { name: 'Qom', birthDate: '02/19/2010' },
+//     { name: 'Rom', birthDate: '01/09/2010' },
+//     { name: 'Som', birthDate: '03/21/2010' },
+//     { name: 'Dom', birthDate: '04/22/2010' },
+// ]
