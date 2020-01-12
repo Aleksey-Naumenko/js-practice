53efbbb8fd537d27d527ec8ebbd75c0c3a16c5c6
Aleksey <aleksey_naumenko@ukr.net>
2020-01-12 23:52:33 +0200

    1

---
+++ lesson27/task1/index.js
@@ -0,0 +1,15 @@
+function getLocalStorageData() {
+    return Object.entries(localStorage)
+        .reduce((acc, [key, value]) => {
+            let newValue;
+            try {
+                newValue = JSON.stringify(value);
+            } catch (e) {
+                newValue = value;
+            }
+            return {
+                ...acc,
+                [key]: newValue,
+            }
+        }, {});
+}
