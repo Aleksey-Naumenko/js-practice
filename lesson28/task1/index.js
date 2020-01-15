f3bd5859fb70eacd1fe60700bb093fea787498b0
Aleksey <aleksey_naumenko@ukr.net>
2020-01-15 11:31:25 +0200

    1

---
+++ lesson28/task1/index.js
@@ -0,0 +1,26 @@
+const culc = num => {
+    let result = num;
+
+    const calculator = {
+        add(value) {
+            result += value;
+            return this;
+        },
+        mult(value) {
+            result *= value;
+            return this;
+        },
+        substract(value) {
+            result -= value;
+            return this;
+        },
+        div(value) {
+            result /= value;
+            return this;
+        },
+        result() {
+            return result;
+        }
+    }
+    return calculator;
+}
