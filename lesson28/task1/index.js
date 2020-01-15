e1816f6575f8f9a71cfaf60df1a995321a05458b
Aleksey <aleksey_naumenko@ukr.net>
2020-01-15 11:35:52 +0200

    1

--- lesson28/task1/index.js
+++ lesson28/task1/index.js
@@ -10,7 +10,7 @@ export const culc = num => {
             result *= value;
             return this;
         },
-        substract(value) {
+        subtract(value) {
             result -= value;
             return this;
         },
