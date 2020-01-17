1f03beafc42216ab08cd7fb4a63d3a80033c1e59
Aleksey <aleksey_naumenko@ukr.net>
2020-01-17 13:38:37 +0200

    1

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -7,25 +7,30 @@ export function addImage(src, callback) {
 
 
     const onImageLoaded = () => {
-        const { width, height } = imgElem;
+        // const { width, height } = imgElem;
         callback(null, { width: 200, height: 100 });
     }
 
     const onErrorLoad = () => {
-        callback('Image load failed');
+        if (error) {
+            callback('Image load failed');
+            return;
+        }
     }
     imgElem.addEventListener('load', onImageLoaded);
     imgElem.addEventListener('error', onErrorLoad);
 }
 
-const onImageLoaded = (error, data) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
-    const { width, height } = data;
-    const sizeElem = document.querySelector('.image-size');
-    sizeElem.textContent = `${width} x ${height}`
-}
 
-addImage(src, onImageLoaded);
+
+// const onImageLoaded = (error, data) => {
+//     if (error) {
+//         console.log(error);
+//         return;
+//     }
+//     const { width, height } = data;
+//     const sizeElem = document.querySelector('.image-size');
+//     sizeElem.textContent = `${width} x ${height}`
+// }
+
+// addImage(src, onImageLoaded);
