c13533621bb90f4046f4ca0480f37f7ab2bda4b3
Aleksey <aleksey_naumenko@ukr.net>
2020-01-18 15:54:13 +0200

    1

--- lesson29/task1/index.js
+++ lesson29/task1/index.js
@@ -5,28 +5,17 @@ export const addImage = (url, callback) => {
     imgElem.setAttribute('alt', 'My photo');
     pageElem.append(imgElem);
 
-    console.log(imgElem);
-
-
     const onImageLoad = () => {
         callback(null, { width: 200, height: 100 });
-        console.log('Must work');
     }
 
-    const onErrorAppear = () => {
-            callback('Image load failed');
+    const onErrorAppear = (error) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
     }
 
     imgElem.addEventListener('load', onImageLoad);
-    imgElem.addEventListener('error', onErrorAppear);
+    imgElem.addEventListener('error', () => onErrorAppear('Image load failed'));
 }
-
-const onImageLoaded = (error, data) => {
-
-    if (error) {
-        console.log(error);
-        return;
-    }
-}
-
-addImage('https://klike.net/uploads/posts/2019-01/1547367999_1.jp', onImageLoaded);
