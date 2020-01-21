ea890a7209a02e7d1355f55dae27a397801ac0d6
Aleksey <aleksey_naumenko@ukr.net>
2020-01-21 18:10:25 +0200

    2

---
+++ lesson31/task2/index.js
@@ -0,0 +1,19 @@
+export const asyncCalculator = numbr => new Promise((resolve) => {
+    setTimeout(() => {
+        console.log(`Initial value: ${numbr}`);
+        resolve(numbr);
+    }, 500);
+})
+    .then(value => new Promise((resolve) => {
+        setTimeout(() => {
+            const squaredNumber = value * value;
+            console.log(`Squared value: ${squaredNumber}`);
+            resolve(squaredNumber);
+        }, 500);
+    }))
+    .then(value => {
+        const squaredNumber = value * 2;
+        console.log(`Doubled value: ${squaredNumber}`);
+    });
+
+asyncCalculator(5);
