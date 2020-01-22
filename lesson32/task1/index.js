960ef05eccb0d4709e73922e9245c1f5d65253c1
Aleksey <aleksey_naumenko@ukr.net>
2020-01-22 21:15:09 +0200

    1

--- lesson32/task1/index.js
+++ lesson32/task1/index.js
@@ -6,14 +6,17 @@ const getValueWithDelay = (value, delay) => new Promise(resolve => {
 })
 
 const asyncNum1 = getValueWithDelay(10, 1000);
-const asyncNum2 = getValueWithDelay(20, 2000);
-const asyncNum3 = getValueWithDelay(30, 3000);
+const asyncNum2 = getValueWithDelay(undefined, 2000);
+const asyncNum3 = getValueWithDelay('30', 3000);
 
 
 const getSum = numbers =>
-    numbers.reduce((acc, num) => acc + num, 0);
+    numbers
+        .filter(value => !isNaN(value))
+        .reduce((acc, num) => acc + Number(num), 0);
 
 export const asyncSum = (...asyncNumbers) => {
-    Promise.all(asyncNumbers)
-        .then(numbers => getSum(numbers));
+    return Promise.all(asyncNumbers)
+        .then(numbers => getSum(numbers))
+        .catch(() => Promise.reject(new Error('Can\'t calculate')));
 }
