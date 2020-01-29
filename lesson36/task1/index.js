1368dc368157cb790eb8732ace3962e678f17cce
Aleksey <aleksey_naumenko@ukr.net>
2020-01-29 19:55:31 +0200

    1

--- lesson36/task1/index.js
+++ lesson36/task1/index.js
@@ -1,13 +1,16 @@
 export const fetchUser = async userId => {
-
-    const userObj = await fetch(`https://api.github.com/users/${userId}`)
-
-    if (!userObj.ok) {
-        return null;
+    try {
+        const userObj = await fetch(`https://api.github.com/users/${userId}`)
+        if (!userObj.ok) {
+            return null;
+        }
+        const userData = await userObj.json();
+        return userData;
+    } catch (e) {
+        throw new Error('Can\'t get User data');
     }
-    
-    const userData = await userObj.json();
-    return userData;
 }
 
-fetchUser('github');
+fetchUser('github')
+    .then(userData => console.log(userData))
+    .catch(e => alert(e.message));
