687a165f36aa45c10218e5c1cf646931ae48eeba
Aleksey <aleksey_naumenko@ukr.net>
2020-01-29 22:03:30 +0200

    hw1

---
+++ lesson36/hw1/index.js
@@ -0,0 +1,19 @@
+const getUserBlog = userId => fetch(`https://api.github.com/users/${userId}`)
+        .then(response => response.json());
+
+
+export const getUsersBlogs = async arrOfUsersId => {
+    const q = arrOfUsersId.reduce((acc, user) => {
+        return [
+            ...acc,
+            getUserBlog(user),
+        ]
+    }, []);
+
+    const allUsers = await Promise.all(q);
+    const arrOfLinks = allUsers.map( ({ blog }) => blog);
+    return arrOfLinks;
+}
+
+getUsersBlogs(['google', 'facebook', 'gaearon'])
+    .then(linksList => console.log(linksList));
